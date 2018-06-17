f31b0a933f874b8898f0d141b2482ce27c89d594
dgketchum <dgketchum@gmail.com>
2018-06-17 10:13:39 -0600

    passing tests on multiple stations comparison of daily tmax

--- tests/test_agrimet_gridmet_comp.py
+++ tests/test_agrimet_gridmet_comp.py
@@ -2,32 +2,37 @@ import unittest
 from met.agrimet import Agrimet
 from met.thredds import GridMet
 from datetime import datetime
+from numpy import nanmean
 
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.fetch_site = 'drlm'
+        self.site_ids = ['robi', 'hntu', 'mdxo', 'mdso', 'masw']
         self.start = '2015-01-01'
         self.end = '2015-12-31'
         self.lat = 46.34
         self.lon = -112.77
 
     def test_agrimet_gridmet(self):
-        agrimet = Agrimet(station=self.fetch_site, start_date=self.start,
-                          end_date=self.end, interval='daily')
 
-        formed = agrimet.fetch_data()
-        agri_tmax = formed['MX'].values
+        for site in self.site_ids:
 
-        s, e = datetime.strptime(self.start, '%Y-%m-%d'), \
-               datetime.strptime(self.end, '%Y-%m-%d')
-        gridmet = GridMet('tmmx', start=s, end=e,
-                          lat=self.lat, lon=self.lon)
-        gridmet_tmax = gridmet.get_point_timeseries()
-        gridmet_tmax = (gridmet_tmax - 273.15).values
-        difference = (gridmet_tmax - agri_tmax) / agri_tmax
-        self.assertLess(difference.mean(), 0.1)
+            agrimet = Agrimet(station=site, start_date=self.start,
+                              end_date=self.end, interval='daily')
+
+            formed = agrimet.fetch_data()
+            agri_tmax = formed['MX'].values
+
+            s, e = datetime.strptime(self.start, '%Y-%m-%d'), \
+                   datetime.strptime(self.end, '%Y-%m-%d')
+            gridmet = GridMet('tmmx', start=s, end=e,
+                              lat=self.lat, lon=self.lon)
+            gridmet_tmax = gridmet.get_point_timeseries()
+            gridmet_tmax = (gridmet_tmax - 273.15).values
+            difference = (gridmet_tmax - agri_tmax) / agri_tmax
+            print(site)
+            self.assertLess(nanmean(difference), 0.1)
 
 
 if __name__ == '__main__':
