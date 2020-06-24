eecd19ebfc7a3a444517df7d83bad22ecda91420
dgketchum <pdWs7kyy>
2020-06-24 15:53:39 -0600

    get_area_timeseries is getting array but not conform

--- utils/process_gridmet.py
+++ utils/process_gridmet.py
@@ -15,60 +15,117 @@
 # ===============================================================================
 from __future__ import print_function, absolute_import
 
-from future.standard_library import hooks
-
 import os
 from calendar import monthrange
 from datetime import datetime
 
 from numpy import mean
 from fiona import open as fiona_open
+from rasterio import open as raster_open
+from rasterio.mask import mask
+from shapely.geometry import Polygon
 
 from met.thredds import GridMet
-from bounds import GeoBounds
-
-home = os.path.expanduser('~')
-LOLO_BOUNDS = GeoBounds(west=-114.8, east=-113.85,
-                        south=46.5, north=46.9)
-
-NETCDF = os.path.join(home, 'IrrigationGIS', 'lolo', 'gridmet_ncdf')
-TIF = os.path.join(home, 'IrrigationGIS', 'lolo', 'tif')
-CATCHMENTS = os.path.join(home, 'IrrigationGIS', 'lolo', 'shp', 'Lolo_WB_Model_Calibration_Catchments_wgs.shp')
-YEARS = [x for x in range(2014, 2020)]
+from bounds import RasterBounds, GeoBounds
+from sat_image.image import Landsat8
 
 
-def get_feature(_id):
-    with fiona_open(CATCHMENTS, 'r') as src:
-        for f in src:
-            if f['properties']['Id'] == _id:
-                catchment = f['geometry']
-                return catchment
-        raise NotImplementedError()
+class CatchmetGridmet():
 
+    def __init__(self, year, month, _id, variable='pr'):
+        home = os.path.expanduser('~')
+        self.netcdf = os.path.join(home, 'IrrigationGIS', 'lolo', 'gridmet_ncdf')
+        self.tif = os.path.join(home, 'IrrigationGIS', 'lolo', 'tif')
+        self.landsat = os.path.join(home, 'IrrigationGIS', 'lolo', 'LC80410282016158LGN01')
+        self.catchments = os.path.join(home, 'IrrigationGIS', 'lolo', 'shp', 'Lolo_WB_Model_Calibration_Catchments_32611.shp')
+        self.project_crs = {'init': 'epsg:32611'}
 
-def get_monthly_gridmet(year, month, _id, variable='pr'):
+        self.year = year
+        self.month = month
+        self.id = _id
+        self.variable = variable
+        self.tif_name = os.path.join(self.tif, 'F{}_{}_{}_{}.tif'.format(_id, variable, year, month))
+        self.catchment = self._get_feature()
 
-    catchment = get_feature(_id)
-    days = monthrange(year, month)[1]
-    s, e = datetime(year, month, 1), datetime(year, month, days)
-    gridmet = GridMet(variable, start=s, end=e, bbox=LOLO_BOUNDS, clip_feature=catchment)
+        self.arr = None
+        self.geometry = None
+        self.gridmet = None
 
-    if variable == 'pr':
-        path = os.path.join(NETCDF, '{}_{}.nc'.format(variable, year))
-        arr = gridmet.get_area_timeseries(file_url=path)
-        pr_sum = arr.sum(axis=0)
+    def _get_feature(self):
+        with fiona_open(self.catchments, 'r') as src:
+            assert src.crs == self.project_crs
+            for f in src:
+                if f['properties']['Id'] == self.id:
+                    catchment = Polygon(f['geometry']['coordinates'][0])
+                    return catchment
+            raise NotImplementedError()
 
-    elif variable == 'temp':
-        path = os.path.join(NETCDF, 'tmmx_{}.nc'.format(year))
-        max = gridmet.get_area_timeseries(file_url=path)
-        path = os.path.join(NETCDF, 'tmmn_{}.nc'.format(year))
-        min = gridmet.get_area_timeseries(file_url=path)
-        daily_avg_temp = mean((min, max), keepdims=True)
+    def _get_landsat_image(self):
+        l8 = Landsat8(self.landsat)
+        bounds = RasterBounds(affine_transform=l8.rasterio_geometry['transform'], profile=l8.rasterio_geometry)
+        proj_bounds = bounds.to_epsg(32611)
+        clip = proj_bounds.get_shapely_polygon()
+        return l8.rasterio_geometry, bounds, clip
 
-    else:
-        raise NotImplementedError()
+    def _clip_to_catchment(self):
+
+        self.gridmet.save_raster(self.arr, self.geometry, self.tif_name)
+
+        with raster_open(self.tif_name) as src:
+            out_arr, out_trans = mask(src, [self.catchment], crop=True, all_touched=True)
+            out_meta = src.meta.copy()
+            out_meta.update({'driver': 'GTiff',
+                             'height': out_arr.shape[1],
+                             'width': out_arr.shape[2],
+                             'transform': out_trans})
+
+        with raster_open(self.tif_name, 'w', **out_meta) as dst:
+            dst.write(out_arr)
+
+        return out_arr
+
+    def get_monthly_gridmet(self):
+
+        self.geometry, bounds, clip = self._get_landsat_image()
+        assert self.geometry['crs'].data == self.project_crs
+
+        days = monthrange(self.year, self.month)[1]
+        s, e = datetime(self.year, self.month, 1), datetime(self.year, self.month, days)
+
+        kwargs = dict(start=s, end=e, bbox=bounds, clip_feature=clip, target_profile=self.geometry)
+
+        if self.variable == 'pr':
+            self.gridmet = GridMet(self.variable, **kwargs)
+            path = os.path.join(self.netcdf, '{}_{}.nc'.format(self.variable, self.year))
+            arr = self.gridmet.get_area_timeseries(file_url=path)
+            self.arr = arr.sum(axis=0)
+
+        elif self.variable == 'temp':
+            path = os.path.join(self.netcdf, 'tmmx_{}.nc'.format(self.year))
+            self.gridmet = GridMet('tmmx', **kwargs)
+            max = self.gridmet.get_area_timeseries(file_url=path)
+
+            path = os.path.join(self.netcdf, 'tmmn_{}.nc'.format(self.year))
+            self.gridmet = GridMet('tmmn', **kwargs)
+            min = self.gridmet.get_area_timeseries(file_url=path)
+
+            self.arr = mean([min, max], axis=0)
+
+        elif self.variable == 'elev':
+            self.gridmet = GridMet(self.variable, **kwargs)
+            path = os.path.join(self.netcdf, 'metdata_elevationdata.nc'.format(self.variable))
+            self.arr = self.gridmet.get_data_subset(file_url=path)
+
+        else:
+            raise NotImplementedError()
+
+        arr = self._clip_to_catchment()
+
+        return arr
 
 
 if __name__ == '__main__':
-    get_monthly_gridmet(2014, 1, _id=1, variable='temp')
+    gridmet = CatchmetGridmet(2014, 1, _id=8, variable='elev')
+    array = gridmet.get_monthly_gridmet()
+    pass
 # ========================= EOF ====================================================================
