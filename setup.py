a3bf5753458ccf28aad17154217ccebf9633c6f7
dgketchum <pdWs7kyy>
2020-06-24 16:27:03 -0600

    get_area_timeseries is getting array but not conform

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(name='metio',
       test_suite='tests.test_suite.suite',
       install_requires=['numpy', 'geopy', 'pandas', 'requests', 'fiona',
                         'future', 'xarray', 'pyproj', 'rasterio', 'xlrd',
-                        'SatelliteImage', 'bs4', 'netcdf4', 'refet'],
+                        'SatelliteImage', 'bs4', 'netcdf4', 'refet', 'bounds'],
       **setup_kwargs)
 
 
